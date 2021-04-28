e4440ad38e1e98c7f7dc260c634f7c688a8ee530
Jose Luis Moscoso <jose.moscoso@duke.edu>
2021-04-28 19:57:05 +0000

    add predict function into main.py

--- main.py
+++ main.py
@@ -8,24 +8,10 @@ logging.basicConfig(level=logging.INFO)
 
 app = Flask(__name__)
 # Load model, scaler, and column transformer
-m = pickle.load(open('model/model_lr.pkl'.'rb'))
+model = pickle.load(open('model/model_lr.pkl'.'rb'))
 scaler = pickle.load(open('model/scaler.pkl'.'rb'))
-ct = pickle.load(open('model/column_transformer.pkl'.'rb'))
+transformer = pickle.load(open('model/column_transformer.pkl'.'rb'))
 
-@app.route('/')
-def hello():
-    """Return a friendly HTTP greeting."""
-    print("I am inside hello world")
-    return 'Hello World! CD'
-
-@app.route('/echo/<name>')
-def echo(name):
-    print(f"This was placed in the url: new-{name}")
-    val = {"new-name": name}
-    return jsonify(val)
-
-@app.route('/predict', methods = ['POST'])
-# Make prediction
 def make_prediction(model, data, scaler, transformer):    
     x_new = data.values
     
@@ -42,6 +28,28 @@ def make_prediction(model, data, scaler, transformer):
     new_preds = ['Not at Risk' if pred == 0 else 'At Risk' for pred in new_preds]
     
     return new_preds
+
+@app.route('/')
+def hello():
+    """Return a friendly HTTP greeting."""
+    print("I am inside hello world")
+    return 'Hello World! CD'
+
+@app.route('/echo/<name>')
+def echo(name):
+    print(f"This was placed in the url: new-{name}")
+    val = {"new-name": name}
+    return jsonify(val)
+
+@app.route('/predict', methods = ['POST'])
+# Make prediction
+def predict():
+    json_payload = request.json
+    LOG.info(f"JSON payload: {json_payload}")
+    data = pd.DataFrame(json_payload)
+    LOG.info(f"inference payload DataFrame: {inference_payload}")
+    prediction = make_prediction(model, data, scaler, transformer)
+    return jsonify({'prediction': prediction})
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
