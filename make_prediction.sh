36e4903b3b253bc492b4735aede8a9a2ac35a120
Varun <vpmavgamer@gmail.com>
2021-04-29 01:59:45 +0000

    Updated main and prediction file

--- make_prediction.sh
+++ make_prediction.sh
@@ -1,45 +1,43 @@
 #!/usr/bin/env bash
-
 PORT=8080
 echo "Port: $PORT"
-
 # POST method predict
 curl -d '{
     "age":{
-        "0":"35"
+        "0":49
     },
     "sex":{
-        "0":"female"
+        "0":"male"
     },
     "cp":{
         "0":"non-anginal pain"
     },
     "trestbps":{
-        "0":"145"
+        "0":118
     },
     "chol":{
-        "0":"200"
+        "0":149
     },
     "fbs":{
         "0":"<120 mg/dl"
     },
     "restecg":{
-        "0":"normal"
+        "0":"left ventricular hypertrophy"
     },
     "thalach":{
-        "0":"160"
+        "0": 126
     },
     "exang":{
-        "0":"yes"
+        "0":"no"
     },
     "oldpeak":{
-        "0":"1.0"
+        "0":0.8
     },
     "slope":{
-        "0":"flat"
+        "0":"upsloping"
     },
     "ca":{
-        "0":"0"
+        "0":3
     },
     "thal":{
         "0":"normal"
